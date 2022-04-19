4fdfcb84f2b4146e8392dfa749a20fcee8586e19
Yeonjoo7 <jenny.likes.breeze@gmail.com>
2022-04-19 23:39:46 +0900

    [ADD] 제품 디테일 정보 가져오는 기능 추가

--- models/productDao.js
+++ models/productDao.js
@@ -1,9 +1,42 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-
+const getProductDetail = async (product_id) => {
+  return await prisma.products.findUnique({
+    select: {
+      name: true,
+      image_url: true,
+      price_before: true,
+      price_after: true,
+      Brands: {
+        select: {
+          id: true,
+          name: true
+        }
+      },
+      Reviews: {
+        select: {
+          id: true,
+          rating: true,
+          content: true,
+          image: true,
+          user_id: true,
+          product_id: true,
+          Reviews_likes: {
+            select: {
+              id: true
+            }
+          }
+        }
+      }
+    },
+    where: {
+      id: Number(product_id)
+    }
+  })
+}
 
 
 module.exports = {
-
+  getProductDetail
 };
