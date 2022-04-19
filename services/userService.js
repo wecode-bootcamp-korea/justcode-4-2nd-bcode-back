d388146d74fd17d11093d8ff240a4fbd04d9a7fb
jeongseoksong <blacksjs@naver.com>
2022-04-20 02:08:45 +0900

    ADD : signUp

--- services/userService.js
+++ services/userService.js
@@ -3,6 +3,25 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
+const signUp = async (username, email, password) => {
+
+    const user = await userDao.getUserEmail(email)
+
+    if (user.length !== 0) {
+        const error = new Error('EXISTING_EMAIL')
+        error.statusCode = 409
+        throw error
+    }
+
+    const encryptedPW = bcrypt.hashSync(password, bcrypt.genSaltSync())
+    const newUser = await userDao.createuUser(username, email, encryptedPW)
+    return newUser
+
+}
 
 
-module.exports = {}
+
+module.exports = {
+    signUp
+
+}
