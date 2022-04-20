40145c86ff5a3f969cbfdbd5cbe9f8cc4c5c046f
Yeonjoo7 <jenny.likes.breeze@gmail.com>
2022-04-20 11:49:13 +0900

    [FIX] Schema 파일 수정  - 관계 테이블명 소문자로 전부 수정  - Users테이블에 brand_id 위치 변경

--- prisma/migrations/20220420020703_create_database/migration.sql
+++ prisma/migrations/20220420020703_create_database/migration.sql
@@ -11,11 +11,11 @@ CREATE TABLE `products` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `image_url` VARCHAR(191) NOT NULL,
+    `brand_id` INTEGER NOT NULL,
     `price_before` DECIMAL(10, 0) NOT NULL,
     `price_after` DECIMAL(10, 0) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `brand_id` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -37,13 +37,14 @@ CREATE TABLE `reviews` (
 -- CreateTable
 CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `deleted_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+    UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
