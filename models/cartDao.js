897f7877e3da315018a9f10b1152c6ea493ab7bb
Yeonjoo7 <jenny.likes.breeze@gmail.com>
2022-04-21 19:57:32 +0900

    Add : 장바구니 추가, 삭제 기능 구현

--- models/cartDao.js
+++ models/cartDao.js
@@ -2,9 +2,66 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 
+const getCurrentCart = async (user_id) => {
+  return await prisma.carts.findMany({
+    select: {
+      quantity: true,
+      products: {
+        select: {
+          id: true,
+          name: true,
+          image_url: true,
+          brands: {
+            select: {
+              name: true
+            }
+          },
+          price_before: true,
+          price_after: true
+        }
+      }
+    },
+    where: {
+      user_id: user_id
+    }
+  })
+}
 
+const addItemToCart = async (product_id, user_id, quantity) => {
+  return await prisma.carts.create({
+    data: {
+      product_id: product_id,
+      user_id: user_id,
+      quantity: quantity
+    }
+  })
+}
+
+const updateItemInCart = async (product_id, user_id, quantity) => {
+  return await prisma.carts.update({
+    where: {
+      product_id: product_id,
+      user_id: user_id
+    },
+    data: {
+      quantity: quantity
+    }
+  })
+}
+
+const deleteItemFromCart = async (product_id, user_id) => {
+  return await prisma.carts.deleteMany({
+    where: {
+      product_id: product_id,
+      user_id: user_id
+    }
+  })
+}
 
 
 module.exports = {
-
+  getCurrentCart,
+  addItemToCart,
+  updateItemInCart,
+  deleteItemFromCart
 };
--- services/cartService.js
+++ services/cartService.js
@@ -1,8 +1,35 @@
 const cartDao = require("../models/cartDao");
 
+const updateCart = async (product_id, user_id, quantity) => {
+  try {
+    const currentCart = await cartDao.getCurrentCart(user_id)
+    const doesItExist = await Promise.all(currentCart.map((cart) => 
+      { return cart.products.id === Number(product_id)? 1 : 0 }
+    ))
+    
+    console.log(doesItExist)
+    if (doesItExist.indexOf(1) === -1) {
+      await cartDao.addItemToCart(Number(product_id), Number(user_id), Number(quantity))
+    } else {
+      await cartDao.updateItemInCart(Number(product_id), Number(user_id), Number(quantity))
+    }
+    
+    return await cartDao.getCurrentCart(user_id)
+  } catch (error) {
+    throw await error
+  }
+}
 
-
+const deleteItemFromCart = async (product_id, user_id) => {
+  try {
+    await cartDao.deleteItemFromCart(Number(product_id), Number(user_id))
+    return await cartDao.getCurrentCart(user_id)
+  } catch (error) {
+    throw await error
+  }
+}
 
 module.exports = {
-
+  updateCart,
+  deleteItemFromCart
 };
