3ec0acab126ae906f7c2e3562be2672adc2fb53e
standard-song <blacksjs@naver.com>
2022-04-27 15:45:41 +0900

    Fix : modify Token

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -3,7 +3,7 @@ const userService = require('../services/userService');
 
 const validateToken = async (req, res, next) => {
     try {
-        const { token } = await req.headers;
+        const token = await req.headers.authorization;
 
         // const accessToken = await req.headers.authorization;   //포스트맨 bearer token 분할용 추후 삭제 
         // realtoken = accessToken.split(' ')
