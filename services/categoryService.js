e9fba9489a76fd6f268c59a341619816f0d6c41a
standard-song <blacksjs@naver.com>
2022-04-28 11:26:18 +0900

    Fix : 오타수정

--- services/categoryService.js
+++ services/categoryService.js
@@ -4,8 +4,8 @@ const categoryList = async () => {
   return await categorytDao.getCategoryList()
 }
 
-const categoryDetail = async (id, limit, highprice, rowprice, review) => {
-  return await categorytDao.getCategoryDetail(id, limit, highprice, rowprice, review)
+const categoryDetail = async (id, limit, highprice, lowprice, review) => {
+  return await categorytDao.getCategoryDetail(id, limit, highprice, lowprice, review)
 }
 
 module.exports = {
